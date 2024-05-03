27ed0ae5e7b5134d38f151930fa35bfc855188fc
Gabriel Garcia <gabito1966@gmail.com>
2024-05-03 12:16:37 -0300

    Agregado de footer

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,8 @@ import './styles/weatherStyles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     < WheatherApp/>
+    <div>
+      <h3>By GabrielDev 2024©</h3>
+    </div>
   </React.StrictMode>,
 )
